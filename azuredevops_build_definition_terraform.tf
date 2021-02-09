e5f48e6ed0267b5d24be785be93eb8c879876495
Matt White <matt.white@microsoft.com>
2021-02-09 13:29:13 +0000

    Add build validation

--- azuredevops_build_definition_terraform.tf
+++ azuredevops_build_definition_terraform.tf
@@ -31,3 +31,20 @@ resource "azuredevops_build_definition" "tf_plan_apply_cicd_canary" {
     yml_path    = ".azure-pipelines/tf-plan-apply-prod.yml"
   }
 }
+
+# Build definition (pipeline) for the ACI CI/CD - build validation
+resource "azuredevops_build_definition" "tf_build_validation" {
+  project_id = azuredevops_project.eslz.id
+  name       = "terraform-build-validation"
+  path       = "\\"
+
+  ci_trigger {
+    use_yaml = true
+  }
+
+  repository {
+    repo_type   = "TfsGit"
+    repo_id     = azuredevops_git_repository.eslz.id
+    yml_path    = ".azure-pipelines/tf-build-validation.yml"
+  }
+}
